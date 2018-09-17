528aaf3b3f03ad790b45c390634503267509befb
lacorp2513 <lizer132@yahoo.com.mx>
2018-09-17 16:53:51 -0500

    17 September

---
+++ ComputeBMI.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class ComputeBMI{
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    final double KILOGRAMS_PER_POUND = 0.4535923;
+    final double METERS_PER__INCH = 0.0254;
+
+
+    System.out.println("Enter weight in pounds: ");
+    double weight = input.nextDouble();
+
+    System.out.println("Enter height in inches");
+    double height = input.nextDouble();
+
+    double weightInKilograms = weight * KILOGRAMS_PER_POUND;
+    double heightInMeters = height * METERS_PER__INCH;
+
+    //double bmi = weightInKilograms / (heightInMeters * //heightInMeters);
+
+    double bmi = weightInKilograms / Math.pow(heightInMeters , 2);
+
+    System.out.println("Your bmi is "+ bmi);
+
+    if (bmi < 16 )
+    System.out.println("Your are seriously underweight");
+    else if( bmi < 18)
+    System.out.println("Your are underweight");
+    else if(bmi < 24)
+    System.out.println("Your are normal weight");
+    else if(bmi < 29)
+    System.out.println("Your are overweight");
+    else if(bmi < 35)
+    System.out.println("Your are seriously overweight");
+    else
+    System.out.println("Your are gravely overweight");
+
+
+
+
+
+
+
+
+
+
+  }
+}
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,39 @@
+import java.util.*;
 public class HelloWorld{
   public static void main(String[] args){
-    int i = 3;
-    i++;
-    System.out.println(i);
-    ++i;
-    System.out.println(i);
-    System.out.println(++i);
-    System.out.println(i++);
-    System.out.println(i);
+  /*int [ ] [ ] sampleArrayC = new int [3] [4];
+  int sampleArrayB [3] [4] = new int;
+  int sampleArrayG [ ] [ ] = new int [3] [ ];
+  int sampleArrayA [ ] [ ] = new int [3] [4];
+  int sampleArrayE [ ] [ ] = {{1, 2}, { 3, 4}};*/
+
+  /*int [][] sample = new int [3][4];
+  for (int i = 0; i < 3;  i++ ) {
+    sample[i][i+1] = 2;
+  }
+  int sum = 0;
+  for (int  i= 0; i < 3 ;  i ++) {
+    for (int  j = 0; j < 3   ; j++ ) {
+      sum += sample[i][j];
+    }
+  }
+  System.out.println(sum);*/
+
+  //Object [] o = {"Song Book",35.95, new Guitar (), 6};
+  String [] s3 = new String[5];
+  //String [5] s1 = new String [];
+  //String [] = {“str1”, “str2”};
+
+  /*int [] s = new int[5];
+  for (int i = 0; i < s.length; i++ ) {
+    s[i]  = s.length -1;
+  }
+  System.out.println(s[3]);*/
+
+  int s[] = {1,2,3,4,5,6};
+
+System.out.println(s.length);
+
+
   }
 }
